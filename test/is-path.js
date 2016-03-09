2e5bfaf341a5d5605a7d50c0b96215ad50b2d059
Rhett Lowe <rng2ml@gmail.com>
2016-03-09 12:09:46 -0800

    Commented and tested isPath -- may extract later

--- test/is-path.js
+++ test/is-path.js
@@ -4,7 +4,36 @@ var assert = require('assert');
 var isPath = require('../lib/is-path');
 
 describe('is-path', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
+  describe('*nix Paths', function () {
+    it('should accept relative paths', function () {
+      assert(isPath('../hello/world'), 'failed with parent dir');
+      assert(isPath('hello/world'), 'failed with self implicit');
+      assert(isPath('./hello/world'), 'failed with self explicit');
+    });
+    it('should accept absolute paths', function () {
+      assert(isPath('/hello/world'));
+    });
+    it('should reject HTML', function () {
+      assert(!isPath('<h1>Hello World</h1>'));
+    });
+    it('should accept file URI scheme', function () {
+      assert(isPath('file://hello/world'));
+    });
+  });
+  describe('Windows Paths', function () {
+    it('should accept relative paths', function () {
+      assert(isPath('..\\hello\\world'), 'failed with parent dir');
+      assert(isPath('hello\\world'), 'failed with self implicit');
+      assert(isPath('.\\hello\\world'), 'failed with self explicit');
+    });
+    it('should accept absolute paths', function () {
+      assert(isPath('\\hello\\world'));
+    });
+    it('should reject HTML', function () {
+      assert(!isPath('<h1>Hello World</h1>'));
+    });
+    it('should accept file URI scheme', function () {
+      assert(isPath('file:///c|hello/world'));
+    });
   });
 });
